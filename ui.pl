2076a83d0d241167d42b57e8826d74bd50133c82
Tomas-Simoes <tomas20simoes@gmail.com>
2025-05-31 19:40:46 +0100

    fixed remove input bug

--- ui.pl
+++ ui.pl
@@ -1,4 +1,4 @@
-:- module(ui, [main_menu/0, game_ui/1, update_game_ui/2]).
+:- module(ui, [main_menu/0, game_ui/1, update_game_ui/2, finished_game/2]).
 
 :-use_module(library(ansi_term)).
 :- use_module(wordle_game).
@@ -6,13 +6,15 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Main Menu
+% main_menu: writes main menu and uses handle_choice/1 to handle the user input
 
 main_menu :- 
     tty:menu("Welcome to UBIdle", 
         [
             start: "Start Game",
             solver: "Start Wordle Solver",
-            options: "Options"
+            options: "Options",
+            exit: "Exit"
         ], Choice),
     handle_choice(Choice).
 
@@ -26,42 +28,87 @@ handle_choice(solver) :-
 handle_choice(options) :-
     writeln("Showing options...").
 
+handle_choice(exit) :-
+    halt.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Game Interface
-    
-game_ui(Formatted) :-
+% game_ui/1:          outputs the game window when the game is initialized and waits for user input
+%                     returns the user input "NextWord" as an array of chars
+
+% update_game_ui/2:   outputs the game window after some user try with the previous user attemps 
+%                     expects feedback of format [UserGuess, [incorrect_letter, correct_letter, ...]]
+%                     returns the user input "NextWord" as an array of chars
+
+% update_backspace/3: outputs the game window after the user backspaces it's input 
+%                     expects feedback (same format as above) and the new user input as an array of chars
+%                     unifies NextWord with the next input
+
+% finished_game/2:    outputs the menu after the user has guessed the word
+%                     expects the final word and the feedback (same format as above) 
+
+game_ui(NextWord) :-
+    tty_clear,
     writeln("Welcome to today's word!"),
     writeln("_  _  _  _  _"),
     write("Choose your word: "),
+
     read_from_ui([], [], Input_Word),
     string_lower(Input_Word, Lower_Word),
-    string_chars(Lower_Word, Formatted).
+    string_chars(Lower_Word, NextWord).
 
 update_game_ui(Feedback, NextWord) :-
     tty_clear,
     writeln("Welcome to today's word!"),
-
     write_feedbacks(Feedback),
-    writeln(""),
-
+    writeln("_  _  _  _  _"),
     write("Choose your word: "),
+
     read_from_ui(Feedback, [], Input_Word),
     string_lower(Input_Word, Lower_Word),
     string_chars(Lower_Word, NextWord).
 
-update_after_backspace(Feedback, NewChars, NextWord) :-
+update_backspace(Feedback, NewChars, NextWord) :-
     tty_clear,
     writeln("Welcome to today's word!"),
 
     write_feedbacks(Feedback),
-    writeln(""),
-
+    writeln("_  _  _  _  _"),
     write("Choose your word: "),
-    write(NewChars),
-    read_from_ui(Feedback, NewChars, Input_Word),
-    string_lower(Input_Word, Lower_Word),
-    string_chars(Lower_Word, NextWord).
+    string_chars(CurString, NewChars),
+    write(CurString),
+
+    read_from_ui(Feedback, NewChars, NextWord).
+
+finished_game(Word, Feedback) :-
+    length(Feedback, FB_Length),
+    NumTries is FB_Length + 1,
+    string_chars(String_Word, Word),
+    
+    format(atom(Title), "Congratulations, you found the word '~w' in ~w tries.~nWelcome to UBIdle", [String_Word, NumTries]),
+    tty:menu(Title, 
+        [
+            menu: "Back to menu",
+            exit: "Exit"
+        ], Choice),
+    handle_finished_choice(Choice).
+
+handle_finished_choice(menu) :-
+    tty_clear,
+    main_menu.
+
+handle_finished_choice(exit) :-
+    halt.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Utils
+% write_feedbacks/1:    outputs the _ _ _ _ _ with correct letters and colors  
+%                       expects a feedback of type [Word, [letters_feedback]]
+
+% read_from_ui/3:       reads character by character and handles specific cases
+%                       expects feedback (same format as above), an array of chars (with the user input)
+%                       returns Final_Input as a string
 
 write_feedbacks([]).
 write_feedbacks([[[],[]]|RestFeedbacks]) :-
@@ -77,28 +124,27 @@ write_feedbacks([[[Letter|Word], [L_Feedback|W_Feedback]]|RestFeedbacks]) :-
 read_from_ui(Feedbacks, Chars, Final_Input) :-
     get_single_char(Code),
     length(Chars, InputLength),
-    
     ( 
     (Code =:= 10; Code =:= 13) ->                       % pressed enter
         ((InputLength < 5) ->
-            read_from_ui([], Chars, Final_Input)
+            read_from_ui(Feedbacks, Chars, Final_Input)
         ;
             string_chars(Final_Input, Chars))
     ;
     (Code =:= 8 ; Code =:= 127) ->                      % pressed backspace 
       tty_clear,
       remove_last(Chars, New_Chars),
-      update_after_backspace(Feedback, New_Chars, Final_Input)
+      update_backspace(Feedbacks, New_Chars, Final_Input)
     ;
     (Code =:= 32) ->                                    % pressed space
-        read_from_ui([], Chars, Final_Input)
+        read_from_ui(Feedbacks, Chars, Final_Input)
     ;
     (InputLength + 1 > 5) ->                            % already has 5 letters
-        read_from_ui([], Chars, Final_Input)
-    ;                                              % valid input
+        read_from_ui(Feedbacks, Chars, Final_Input)
+    ;                                                    % valid input
         char_code(Char, Code),
         write(Char),
         append(Chars, [Char], New_Chars),
-        read_from_ui([], New_Chars, Final_Input)
+        read_from_ui(Feedbacks, New_Chars, Final_Input)
     ).
     
--- wordle_game.pl
+++ wordle_game.pl
@@ -1,16 +1,31 @@
-:-module(wordle_game, [start_game/0]).
+:- module(wordle_game, [start_game/0]).
 
-:-use_module(utils).
-:-use_module(ui).
- 
+:- use_module(utils).
+:- use_module(ui).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Game Loop
+% start_game/0:          starts the game by getting a random word and launching the game loop
+
+% game_loop/3:           checks if user guessed the word; if not, updates feedback and repeats
+%                        expects word to guess, current user input, and feedback 
+
+% handle_user_input/4:   builds feedback list for the input word 
+%                        expects target word, user word, current index, resulting feedback
+
+% handle_letter/4:       finds index of letter if it exists in the word
+%                        returns -1 if not found
+
+% get_feedback/3:        returns feedback based on index match (correct, misplaced, incorrect)
+
 start_game :-
     get_random_word(Word),
     game_ui(UserWord),
     game_loop(Word, UserWord, []).
 
 game_loop(Word, Word, OverallFeedback) :- 
-    tty_clear,
-    format("Congradulations, you found the word!").
+    finished_game(Word, OverallFeedback).
+
 game_loop(Word, UserWord, OverallFeedback) :- 
     handle_user_input(Word, UserWord, 0, NewFeedback),
     append(OverallFeedback, [[UserWord, NewFeedback]], NewOverallFeedback),
@@ -18,19 +33,19 @@ game_loop(Word, UserWord, OverallFeedback) :-
     game_loop(Word, NextWord, NewOverallFeedback).
 
 handle_user_input(_, [], _, []).
-handle_user_input(Word, [U_Letter | U_Word], U_Index, [W_Feedback|NewAcc]) :-
+
+handle_user_input(Word, [U_Letter | U_Word], U_Index, [W_Feedback | NewAcc]) :-
     handle_letter(Word, U_Letter, 0, W_Index),
     get_feedback(U_Index, W_Index, W_Feedback),
     Next_Index is U_Index + 1,
     handle_user_input(Word, U_Word, Next_Index, NewAcc).
 
 handle_letter([], _, _, -1).
-handle_letter([Letter|_], Letter, W_Index, W_Index).
-handle_letter([_|R_Word], Letter, W_Index, Res) :-
+handle_letter([Letter | _], Letter, W_Index, W_Index).
+handle_letter([_ | R_Word], Letter, W_Index, Res) :-
     New_Index is W_Index + 1,
     handle_letter(R_Word, Letter, New_Index, Res).
 
 get_feedback(_, -1, incorrect).
 get_feedback(Res, Res, correct).
 get_feedback(_, _, misplaced).
-    
